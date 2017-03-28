595925be8d7022241b59db2400a36c9b4cdf87fc
lubien <lubien1996@gmail.com>
2017-03-28 19:43:47 -0300

    Add homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,42 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        NUXT
-      </h1>
-      <h2 class="subtitle">
-        Universal Vue.js Application
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">Github</a>
-      </div>
-    </div>
-  </section>
+  <div>
+    <div id="content_title">Resumo:</div><br/>
+    O Projeto SPIDER - <i>Software Process Improvement: Development and Research</i> -
+    foi institucionalizado em 2009 no Instituto de Ciências Exatas e Naturais da UFPA e
+    possui como corpo integrante alunos: da Graduação da Faculdade de Computação, a partir
+    dos cursos de <a target="_blanck" id="content_link" href="http://www.cultura.ufpa.br/informatica/">
+      Bacharelado em Ciência da Computação</a> e <a target="_blanck" id="content_link" href="http://www.ufpa.br/cbsi">
+      Bacharelado em Sistemas de Informação</a>; do <a target="_blanck" id="content_link" href="http://www.ufpa.br/ppgcc">
+      Doutorado e do Mestrado do PPGCC - Programa de Pós-Graduação em Ciência da Computação</a>
+    ; do <a target="_blanck" id="content_link" href="http://www.ppgee.ufpa.br/">Doutorado do
+      PPGEE – Programa de Pós-Graduação em Engenharia Elétrica</a>; e do <a target="_blanck" id="content_link" href="http://www.cin.ufpe.br/">
+      Mestrado e Doutorado do Centro de Informática da UFPE</a>. Este projeto tem, como um
+    dos focos principais, apresentar um levantamento das ferramentas de software livre com
+    características adequadas para possibilitar a criação de produtos de trabalhos (artefatos)
+    derivados dos resultados esperados descritos nos objetivos dos processos do
+    <a target="_blanck" id="content_link" href="http://www.softex.br/mpsbr">modelo MPS.BR
+      - Melhoria do Processo de Software Brasileiro</a>, mantido pela SOFTEX - Associação para
+    Promoção da Excelência do Software Brasileiro, e das práticas específicas descritas nos
+    objetivos das áreas de processo do <a target="_blanck" id="content_link" href="http://www.sei.cmu.edu/cmmi/">
+      modelo CMMI – Capability Maturity Model Integration</a>, mantido pelo SEI – Software
+    Engineering Institute, o que evidenciam a implementação do programa da qualidade organizacional.
+    <br /><hr /><br />
+
+    <div id="content_title">Palavras-chave:</div><br/>
+    Qualidade de Software; Melhoria Contínua; Processos de Desenvolvimento de Software;
+    Modelos e Normas de Qualidade; Ferramentas de Software Livre; Revisão Sistemática.
+    <br /><hr/><br />
+
+    <div id="content_title">Contatos:</div><br/>
+    Sandro Ronaldo Bezerra Oliveira<br />
+    Coordenador do Projeto SPIDER<br />
+    Instituto de Ciências Exatas e Naturais / Universidade Federal do Pará<br />
+    Rua Augusto Corrêa, 01, Guamá, CEP 66075-110, Caixa postal 479, Belém–PA–Brasil<br />
+    E-mail: ufpa.spider@gmail.com<br/>
+    Fone/Fax (+55 91) 31027405, 32017409<br />
+  </div>
 </template>
 
 <script>
-import Logo from '~components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+  export default {}
 </script>
-
-<style>
-.container
-{
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links
-{
-  padding-top: 15px;
-}
-</style>
