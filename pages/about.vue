5c621a7f920f8dc85548930f69710f1836187ea6
lubien <lubien1996@gmail.com>
2017-03-28 19:53:25 -0300

    About page

---
+++ pages/about.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div id="content_title">Contexto:</div><hr />
+    Devido a globaliza&ccedil;&atilde;o, cada vez mais os produtos e servi&ccedil;os est&atilde;o sujeitos a novas exig&ecirc;ncias de mercado, a alta competitividade e a concorr&ecirc;ncia internacional. Neste cen&aacute;rio, a qualidade torna-se uma arma competitiva, tendo em vista a equipara&ccedil;&atilde;o com padr&otilde;es internacionais, conformidade com a especifica&ccedil;&atilde;o e a satisfa&ccedil;&atilde;o do cliente.
+    <br /><br />
+    A qualidade pode ser definida como a totalidade das caracter&iacute;sticas de uma entidade que lhe confere a capacidade de satisfazer &agrave;s necessidades expl&iacute;citas e impl&iacute;citas. Entre os benef&iacute;cios da qualidade est&atilde;o as redu&ccedil;&otilde;es de defeitos, aumento da confiabilidade do produto, redu&ccedil;&atilde;o do esfor&ccedil;o de re-trabalho, redu&ccedil;&atilde;o de custo de desenvolvimento e manuten&ccedil;&atilde;o, e maior &iacute;ndice de satisfa&ccedil;&atilde;o dos clientes.
+    <br /><br />
+    A literatura tem mostrado que a qualidade do produto ou servi&ccedil;o de uma organiza&ccedil;&atilde;o, s&oacute; &eacute; plenamente obtida se os processos das organiza&ccedil;&otilde;es aderirem &agrave;s normas de qualidade. Visando a qualidade do processo, o modelo MPS.BR – Modelo de Processo de Software Brasileiro, mantido pela SOFTEX - Sociedade para Promo&ccedil;&atilde;o da Excel&ecirc;ncia do Software Brasileiro, e o modelo CMMI – Capability Maturity Model Integration, mantido pelo SEI – Software Engineering Institute, prop&otilde;em-se a serem guias evolucion&aacute;rio para o aperfei&ccedil;oamento e adequa&ccedil;&atilde;o de processos organizacionais.
+    <br /><br />
+    De uma forma geral, o MPS.BR e o CMMI descrevem o qu&ecirc; deve ser feito para melhoria gradual de processos, definindo n&iacute;veis de maturidade que s&atilde;o organizados por processos/&aacute;reas de processo que possuem objetivos alcan&ccedil;ados por resultado esperados/pr&aacute;ticas espec&iacute;ficas os quais s&atilde;o evidenciados em produtos de trabalho. Para tanto, faz-se necess&aacute;ria a utiliza&ccedil;&atilde;o de ferramentas de software para possibilitar a implanta&ccedil;&atilde;o dos modelos MPS.BR e CMMI nas organiza&ccedil;&otilde;es.
+    <br /><br />
+    Atualmente, existe um movimento crescente onde esfor&ccedil;os individuais e coletivos, impulsionados pela massifica&ccedil;&atilde;o da Internet, derivam a cria&ccedil;&atilde;o de softwares n&atilde;o propriet&aacute;rios com o objetivo de atender &agrave;s necessidades comuns das organiza&ccedil;&otilde;es e de indiv&iacute;duos. Estes softwares denominados software livre, devido a liberdade de qualquer um poder utiliz&aacute;-los e modific&aacute;-los, est&atilde;o cada vez mais difundidos nas organiza&ccedil;&otilde;es e no uso dom&eacute;stico. At&eacute; governos, como o brasileiro, criam pol&iacute;ticas de incentivo para difus&atilde;o destes softwares.
+    <br /><br />
+    A defini&ccedil;&atilde;o de software livre (free software) &eacute; o software dispon&iacute;vel com a permiss&atilde;o para qualquer um us&aacute;-lo, copi&aacute;-lo e distribui-lo, seja na forma original ou com modifica&ccedil;&otilde;es, seja gratuitamente ou com custo. A possibilidade de modifica&ccedil;&otilde;es implica que o c&oacute;digo fonte esteja dispon&iacute;vel (open source) e o software deve poder ser executado em um sistema operacional tamb&eacute;m livre.
+    <br /><br /><br />
+
+    <div id="content_title">Justificativa:</div><hr />
+    • a demanda por profissionais e empresas qualificadas tem levado a instala&ccedil;&atilde;o de f&aacute;bricas de software para exporta&ccedil;&atilde;o em diversos pa&iacute;ses do mundo, visando a redu&ccedil;&atilde;o de custos e, principalmente, a falta de m&atilde;o-de-obra qualificada nos pa&iacute;ses de origem;
+    <br /><br />
+    • diversas iniciativas do Governo e do Mercado marcam a transi&ccedil;&atilde;o para uma postura mais agressiva voltada &agrave; exporta&ccedil;&atilde;o de software: o pr&oacute;prio MPS.BR, apoiado pelo Governo; e as recentes pol&iacute;ticas de investimento na qualifica&ccedil;&atilde;o e especializa&ccedil;&atilde;o de profissionais relacionadas com a Secretaria de Pol&iacute;tica de Inform&aacute;tica – SEPIN;
+    <br /><br />
+    • crescente número de acordos de terceiriza&ccedil;&atilde;o envolvendo empresas nacionais e conglomerados estrangeiros;
+    <br /><br />
+    • baixa quantidade de empresas na regi&atilde;o Norte diretamente relacionadas com o desenvolvimento de software. O cen&aacute;rio &eacute; projetado a partir de micro e pequenas empresas de baixa competitividade, estando completamente alheio &agrave; corrida por mercados estrangeiros;
+    <br /><br />
+    • not&oacute;ria capacidade das universidades paraenses em formar profissionais em quantidade e qualidade no setor de desenvolvimento de software, cita-se a grande quantidade de cursos de gradua&ccedil;&atilde;o e p&oacute;s-gradua&ccedil;&atilde;o existentes no mercado local;
+    <br /><br />
+    • os resultados significativos obtidos pelas tr&ecirc;s principais empresas públicas do setor de Tecnologia da Informa&ccedil;&atilde;o no estado do Par&aacute;: a Regional Bel&eacute;m do SERPRO – Servi&ccedil;o Federal de Processamento de Dados obteve em 2005 a avalia&ccedil;&atilde;o no N&iacute;vel 2 do Capability Maturity Model for Software (CMM-Sw); a PRODEPA – Empresa de Processamento de Dados do Estado do Par&aacute; obteve a avalia&ccedil;&atilde;o no n&iacute;vel F do MPS.BR no in&iacute;cio de 2008; o CTIC/UFPA obteve a avalia&ccedil;&atilde;o G do MPS.BR no final de 2008; enquanto que a Equilibrium Web obteve em 2010 a avalia&ccedil;ão N&iacute;vel 6 do MPS.BR. O investimento foi significativo, pois os processos de implanta&ccedil;&atilde;o de qualidade duram, em m&eacute;dia, um ano e, durante a implanta&ccedil;&atilde;o destas iniciativas havia poucos profissionais habilitados para implementa&ccedil;&atilde;o destes modelos no Par&aacute;;
+    <br /><br />
+    • este cen&aacute;rio deve se tornar ainda mais cr&iacute;tico para as empresas locais em decorr&ecirc;ncia da Recomenda&ccedil;&atilde;o do Tribunal de Contas da Uni&atilde;o (TCU) de 2007 na ado&ccedil;&atilde;o do modelo MPS.BR como elemento de pontua&ccedil;&atilde;o diferenciada para a contrata&ccedil;&atilde;o de produtos e servi&ccedil;os na &aacute;rea de software, o que caracteriza ainda mais a necessidade da implementa&ccedil;&atilde;o do programa de melhoria da qualidade dos processos organizacionais;
+    <br /><br />
+    • enfatizando este contexto, de acordo com os relat&oacute;rios de “Resultados de Desempenho das organiza&ccedil;&otilde;es que adotaram o modelo MPS” e de “Li&ccedil;&otilde;es Aprendidas do MPS.BR”, ambos de 2008, a dura&ccedil;&atilde;o de uma implementa&ccedil;&atilde;o &eacute; reduzida substancialmente ao se adotar como pr&aacute;tica ferramentais automatizados, o que torna o processo de implementa&ccedil;&atilde;o mais &aacute;gil e menos custoso para as organiza&ccedil;&otilde;es. Adicionalmente, a partir do grande quantitativo de ferramentas de software livre de apoio &agrave;s disciplinas da Engenharia de Software existentes na comunidade, pode-se caracterizar uma minimiza&ccedil;&atilde;o dos custos ao longo do programa de melhoria da qualidade, quando desta ado&ccedil;&atilde;o.
+    <br /><br /><br />
+
+    <div id="content_title">Objetivos:</div><hr />
+    Neste contexto, o projeto SPIDER tem, como um dos focos principais, apresentar um levantamento das ferramentas de software livre com caracter&iacute;sticas adequadas para possibilitar a cria&ccedil;&atilde;o de produtos de trabalhos (artefatos que evidenciam a implementa&ccedil;&atilde;o do programa da qualidade organizacional) derivados dos resultados esperados/práticas específicas descritos nos objetivos das &aacute;reas de processo do modelo MPS.BR e CMMI.
+    <br /><br />
+    Em decorr&ecirc;ncia deste levantamento, pretende-se especificar e desenvolver um SUITE de ferramentas a fim de propiciar um uso mais integrado das suas fun&ccedil;&otilde;es/opera&ccedil;&otilde;es dispon&iacute;veis, de modo a apoiar a implanta&ccedil;&atilde;o dos processos/&aacute;reas de processo do modelo MPS.BR e CMMI, obedecendo o fluxo de depend&ecirc;ncia proposto por este modelo de qualidade de processo. Isso se evidencia pela caracter&iacute;stica do c&oacute;digo fonte dispon&iacute;vel (open source), para automatizar ao m&aacute;ximo o processo, e evitar re-trabalho e redundância de informa&ccedil;&otilde;es.
+    <br /><br />
+    O objetivo deste projeto visa apresentar alternativas vi&aacute;veis com rela&ccedil;&atilde;o a ferramentas de software para auxiliar a implementa&ccedil;&atilde;o dos modelos MPS.BR e CMMI nas organiza&ccedil;&otilde;es, sem a necessidade de aquisi&ccedil;&atilde;o de softwares propriet&aacute;rios e com a possibilidade da ferramenta ser customizada para atender as especificidades da organiza&ccedil;&atilde;o, diminuindo os custos e o tempo ao longo da implementa&ccedil;&atilde;o deste programa de maturidade.
+    <br /><br />
+    Entretanto, faz-se necess&aacute;rio um experimento real de utiliza&ccedil;&atilde;o de algumas das ferramentas de software livre a serem apresentadas neste projeto para que os resultados obtidos apontem a viabilidade efetiva da ferramenta e os problemas ocorridos.
+    <br /><br />
+    Define-se como premissa, para o desenvolvimento do SUITE, uma arquitetura de integra&ccedil;&atilde;o facilmente adaptada quando do estudo dos resultados esperados para todos os n&iacute;veis de maturidade do mesmo modelo, tornando-o flex&iacute;vel e adapt&aacute;vel para novas demandas de ferramentas automatizadas de apoio a diferentes disciplinas da Engenharia de Software.
+    <br /><br />
+    Vale mencionar, ainda, os inúmeros resultados obtidos a partir de pesquisas em parcerias, que ser&atilde;o mantidos com a institucionaliza&ccedil;&atilde;o deste projeto, com núcleos de renome na &aacute;rea de Engenharia de Software, como o Centro de Inform&aacute;tica da Universidade Federal de Pernambuco, atrav&eacute;s do projeto de pesquisa ImPProS – Um Ambiente de Implementa&ccedil;&atilde;o de Processos de Software, com seus inúmeros artigos nacionais e internacionais publicados, e com o núcleo ProQualiti – Núcleo de Estudos em Engenharia e Qualidade de Software (www.proqualiti.org.br), &oacute;rg&atilde;o destinado &agrave; finalidade de congregar profissionais, estudantes e empres&aacute;rios atuantes nas &aacute;reas de Engenharia e Qualidade de Software, com seus cursos &agrave; distância e sua revista que recebeu o 2º lugar no Pr&ecirc;mio Dorgival Brand&atilde;o Júnior da Qualidade e Produtividade de Software em 2005.
+    <br /><br />
+    N&atilde;o podemos esquecer-nos de mencionar que este projeto possui tamb&eacute;m como foco corroborar no atendimento dos objetivos listados no projeto “Rede Paraense de Pesquisa em Tecnologias de Informa&ccedil;&atilde;o e Comunica&ccedil;&otilde;es (TIC)” no sub-projeto “Laborat&oacute;rio de Tecnologia de Software” aprovado e financiado pela FAPESPA, segundo edital 014/2008.
+    <br /><br /><br />
+
+    <div id="content_title">Metas:</div><hr />
+    • Conjunto de ferramentas de software livre de apoio &agrave; implementa&ccedil;&atilde;o do programa de melhoria da qualidade organizacional;
+    <br /><br />
+    • Integra&ccedil;&atilde;o deste ferramental automatizado a partir de um SUITE;
+    <br /><br />
+    • Capacita&ccedil;&atilde;o e melhoria cont&iacute;nua dos conhecimentos te&oacute;ricos e pr&aacute;ticos dos estudantes dos programas de gradua&ccedil;&atilde;o e p&oacute;s-gradua&ccedil;&atilde;o da Faculdade de Computa&ccedil;&atilde;o da UFPA nas &aacute;reas de Engenharia e Qualidade de Software;
+    <br /><br />
+    • Forma&ccedil;&atilde;o e qualifica&ccedil;&atilde;o de recursos humanos da regi&atilde;o, na &aacute;rea de Qualidade do Processo de Software, a partir das recomenda&ccedil;&otilde;es dos modelos MPS.BR e CMMI e do SUITE desenvolvido, visando garantir a melhoria cont&iacute;nua dos processos de software da organiza&ccedil;&atilde;o e na gest&atilde;o do conhecimento dos ativos organizacionais;
+    <br /><br />
+    • Apoio para que as empresas da regi&atilde;o, existentes ou em cria&ccedil;&atilde;o, que desenvolvam atividades inovadoras em termos tecnol&oacute;gicos (P,D&amp;I) de impacto comercial ou social com qualidade, a partir da implanta&ccedil;&atilde;o deste SUITE de ferramentas de software livre;
+    <br /><br />
+    • Participa&ccedil;&atilde;o efetiva da comunidade acad&ecirc;mica para provimento de melhoria dos processos e produtos organizacionais, e a dissemina&ccedil;&atilde;o de programas de melhoria de qualidade de software nas empresas locais com foco no uso de modelos e normas;
+    <br /><br />
+    • Avalia&ccedil;&atilde;o de processos de empresas locais, regionais e nacionais com vistas a aumentar o n&iacute;vel de maturidade em desenvolvimento de software no Estado do Par&aacute;;
+    <br /><br />
+    • Integra&ccedil;&atilde;o do SUITE de ferramentas a um ambiente de apoio &agrave; defini&ccedil;&atilde;o e gest&atilde;o do processo de software, garantindo assim a plena execu&ccedil;&atilde;o do ciclo de vida do processo de software.
+    <br /><br />
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
