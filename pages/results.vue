67c2d7b49b722bef841bf3cd0cadb7849797cff4
lubien <lubien1996@gmail.com>
2017-03-28 22:42:13 -0300

    Add results page

---
+++ pages/results.vue
@@ -0,0 +1,276 @@
+<template>
+  <div>
+    <script type="text/javascript">
+    // eslint-disable-next-line
+    function setTable (what){
+      if (document.getElementById(what).style.display === 'none') {
+        document.getElementById(what).style.display = 'block'
+      } else if (document.getElementById(what).style.display === 'block') {
+        document.getElementById(what).style.display = 'none'
+      }
+    }
+    </script>
+
+    <a id="content_title2" href="#" onclick="setTable('table1');return false">
+      <div id="content_title">Apoio à Gestão de Projetos+ </div><hr /><br/>
+    </a>
+
+    <table id="table1" style="display:none;">
+
+      <tr><td>
+          <div>
+            •	Ferramenta OpenProj.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMaN1ZVcmtRcXA2bDg&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+            •	Ferramenta Spider-APF.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMaYmg4NVowRGpHVnM&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+            •	Ferramenta Spider-UCP.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMab25sUzlvZ1BOWHc&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+            •	Ferramenta Spider-CoCoMo.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMaOE0zOWlPSndUODA&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+          </div><br/>
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('table2');return false">
+      <div id="content_title"> Apoio à Gestão de Requisitos+</div><hr /><br/>
+	  </a>
+
+    <table id="table2" style="display:none;">
+
+      <tr><td>
+          <div>
+            •	Ferramenta OSRMT.<br/>
+          </div><br/>
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('table3');return false">
+      <div id="content_title"> Apoio à Gestão de Configuração+</div><hr /><br/>
+    </a>
+
+    <table id="table3" style="display:none;">
+      <tr><td>
+          <div>
+            •	Ferramenta Redmine.<br/>
+            •	Ferramenta CVS.<br/>
+            •	Ferramenta SVN.<br/>
+            •	Ferramenta TRAC.<br/>
+          </div><br/>
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('table4');return false">
+      <div id="content_title"> Apoio à Medição+</div><hr /><br/>
+    </a>
+
+    <table id="table4" style="display:none;">
+      <tr><td>
+          <div>
+            •	Ferramenta Spider-MsControl.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMaU2p1Rk5kUFN3NFU&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+          </div><br/>
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('table5');return false">
+      <div id="content_title"> Apoio à Garantia da Qualidade+</div><hr /><br/>
+    </a>
+
+    <table id="table5" style="display:none;">
+      <tr><td>
+          <div>
+            •	Ferramenta Spider-QA.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMaaXZpYkNzTWZYVzA&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+            •	Ferramenta Redmine.<br/>
+          </div><br/>
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('table6');return false">
+      <div id="content_title">  Apoio à Aquisição+</div><hr /><br/>
+    </a>
+
+    <table id="table6" style="display:none;">
+      <tr><td>
+          <div>
+            •	Ferramenta Spider-ACQ.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMaQnJYZTdzbVFGSVE&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+          </div><br/>
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('table7');return false">
+      <div id="content_title"> Apoio à Definição do Processo Organizacional+</div><hr /><br/>
+    </a>
+
+    <table id="table7" style="display:none;">
+      <tr><td>
+          <div>
+            •	Ferramenta Spider-PM.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMaY29CQ1dqWWc0cE0&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+          </div><br/>
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('table8');return false">
+      <div id="content_title">Apoio à Melhoria e Avaliação do Processo Organizacional+ </div><hr /><br/>
+    </a>
+
+    <table id="table8" style="display:none;">
+
+      <tr><td>
+          <div>
+            •	Ferramenta Spider-PM.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMaY29CQ1dqWWc0cE0&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+            •	Ferramenta Spider-PI.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMaRU1FUDhoU0p5UmM&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+          </div><br/>
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('table9');return false">
+      <div id="content_title"> Apoio à Gestão de Reutilização+</div><hr /><br/>
+	  </a>
+
+    <table id="table9" style="display:none;">
+
+      <tr><td>
+          <div>
+            •	Ferramenta Spider-GRU.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMaZ2czU2ZHemthRDg&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+          </div><br/>
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('table10');return false">
+      <div id="content_title"> Apoio à Gestão de Decisão+</div><hr /><br/>
+    </a>
+
+    <table id="table10" style="display:none;">
+      <tr><td>
+          <div>
+            •	Ferramenta Spider-DAR.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMaMllSWng0WTNfaFk&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+          </div><br/>
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('table11');return false">
+      <div id="content_title"> Apoio à Gestão de Riscos+</div><hr /><br/>
+    </a>
+
+    <table id="table11" style="display:none;">
+      <tr><td>
+          <div>
+            •	Ferramenta Spider-RM.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMaLUwtNVowaDJBZlU&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+          </div><br/>
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('table12');return false">
+      <div id="content_title"> Apoio a Critérios Objetivos+</div><hr /><br/>
+    </a>
+
+    <table id="table12" style="display:none;">
+      <tr><td>
+          <div>
+            •	Ferramenta Spider-CL.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMacnpjVzZ1T2FEaG8&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+          </div><br/>
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('table13');return false">
+      <div id="content_title">  Apoio à Avaliação de Modelos de Qualidade+</div><hr /><br/>
+    </a>
+
+    <table id="table13" style="display:none;">
+      <tr><td>
+          <div>
+            •	Ferramenta Spider-Appraisal.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMaeTJLZ1JVdWZJdWc&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+          </div><br/>
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('table14');return false">
+      <div id="content_title"> Apoio à Simulação para o Ensino de Melhoria do Processo de Software+</div><hr /><br/>
+    </a>
+
+    <table id="table14" style="display:none;">
+      <tr><td>
+          <div>
+            •	Ferramenta DesigMPS.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMaV205Ym1kUTZvdzg&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+          </div><br/>
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('table15');return false">
+      <div id="content_title"> Apoio à Execução de Processos de Software+</div><hr /><br/>
+    </a>
+
+    <table id="table15" style="display:none;">
+      <tr><td>
+          <div>
+            •	Ferramenta Spider-PE.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMadUtRVi1FUFQ3YkU&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+          </div><br/>
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('table16');return false">
+      <div id="content_title"> Apoio à Avaliação do Produto+</div><hr /><br/>
+    </a>
+
+    <table id="table16" style="display:none;">
+      <tr><td>
+          <div>
+            •	Ferramenta Spider-PQ.<br/>
+            Para download,<a target="_blanck"  id="content_link" href="https://drive.google.com/folderview?id=0B42oav4rfJMaSElmZnk3TnRYZ0k&usp=sharing" >
+              <i> clique aqui.</i>
+            </a><br /><br />
+          </div><br/>
+        </td></tr>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
