9495b9c7f71f37d5138182f8c20dde9e7406a761
lubien <lubien1996@gmail.com>
2017-03-28 23:29:27 -0300

    Add bookmarks page

---
+++ pages/bookmarks.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <a target="_blanck" id="bookmark" href="http://www.softex.br" >
+		  • Softex
+	  </a><br /><br />
+	  <a target="_blanck" id="bookmark" href="http://www.swebok.org" >
+		  • SWEBOOK
+	  </a><br /><br />
+	  <a target="_blanck" id="bookmark" href="http://www.isospice.com" >
+		  • ISO-Spice
+	  </a><br /><br />
+	  <a target="_blanck" id="bookmark" href="http://www.sei.cmu.edu/cmmi" >
+		  • CMMI
+	  </a><br /><br />
+	  <a target="_blanck" id="bookmark" href="http://www.ieee.org" >
+		  • Institute of Electrical and Electronic Engineers
+	  </a><br /><br />
+	  <a target="_blanck" id="bookmark" href="http://www.softex.br/mpsbr" >
+		  • Modelo de Melhoria do Processo de Software Brasileiro - MPS.BR
+	  </a><br /><br />
+	  <a target="_blanck" id="bookmark" href="http://egps.ufpa.br/index.php/Main_Page" >
+		  • Curso de Especializa&ccedil;&atilde;o em Ger&ecirc;ncia de Projetos de Software
+	  </a><br /><br />
+	  <a target="_blanck" id="bookmark" href="http://www.cientec.net/scripts/controla/download.asp" >
+		  • Controla
+	  </a><br /><br />
+	  <a target="_blanck" id="bookmark" href="http://sourceforge.net/projects/osrmt" >
+		  • osmrt
+	  </a><br /><br />
+	  <a target="_blanck" id="bookmark" href="http://sourceforge.net/projects/openproj" >
+		  • openproj
+	  </a><br /><br />
+	  <a target="_blanck" id="bookmark" href="http://sourceforge.net/projects/dotproject" >
+		  • dotProject
+	  </a><br /><br />
+	  <a target="_blanck" id="bookmark" href="http://sourceforge.net/projects/cvswc" >
+		  • CVS</b>
+	  </a><br /><br />
+	  <a target="_blanck" id="bookmark" href="http://sourceforge.net/projects/svnconf" >
+		  • SVN
+	  </a><br /><br />
+	  <a target="_blanck" id="bookmark" href="http://sourceforge.net/projects/mantisbt" >
+		  • Mantis
+	  </a><br /><br />
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
