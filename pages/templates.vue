62ef8a7f05ff01fac36ca506187927433e9515a8
lubien <lubien1996@gmail.com>
2017-03-28 23:12:20 -0300

    Add templates page

---
+++ pages/templates.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <a download id="content_title2" href="templates/SPIDER_modelo_acordoconfidencialidade.doc" >• Acordo de Confidencialidade</a>
+	  - Template usado pelo grupo para mostrar as cl&aacute;usulas do acordo de particularidade das informa&ccedil;&otilde;es recolhidas.
+	  <br /><br /><br />
+
+    <a download id="content_title2" href="templates/SPIDER_modelo_apresentacao.ppt" >• Apresenta&ccedil;&atilde;o</a>
+	  - Template usado pelo grupo para a realiza&ccedil;&atilde;o de apresenta&ccedil;&otilde;es de informa&ccedil;&otilde;es sobre o projeto de pesquisa.
+	  <br /><br /><br />
+
+    <a download id="content_title2" href="templates/SPIDER_modelo_avaliacaoferramenta.doc" >• Avalia&ccedil;&atilde;o de Ferramenta</a>
+	  - Template usado pelo grupo como instrumento para avaliar ferramentas GPL no contexto do modelo MPS.BR.
+	  <br /><br /><br />
+
+    <a download id="content_title2" href="templates/SPIDER_modelo_banner.ppt" >• Banner</a>
+	  - Template usado pelo grupo para a exposi&ccedil;&atilde;o do projeto de pesquisa.
+	  <br />
+	  <br /><br />
+
+    <a id="content_title2" href="templates/SPIDER_modelo_certificado.doc" >• Certificado</a>
+	  - Template usado pelo grupo para emiss&atilde;o de certificados.
+	  <br />
+	  <br /><br />
+
+    <a download id="content_title2" href="templates/SPIDER_modelo_descricaoprocesso.doc" >• Defini&ccedil;&atilde;o do Modelo de Processo</a>
+	  - Template usado pelo grupo como instrumento para descrever a defini&ccedil;&atilde;o dos modelos de processo constantes nas ferramentas sistematizadas.
+	  <br /><br /><br />
+
+    <a download id="content_title2" href="templates/SPIDER_modelo_descricaosubprojeto.doc" >• Descri&ccedil;&atilde;o do Sub-Projeto</a>
+	  - Template usado pelo grupo para compor a descri&ccedil;&atilde;o dos Sub-Projetos desenvolvidos/em desemvolvimento no projeto SPIDER.
+	  <br /><br /><br />
+
+    <a download id="content_title2" href="templates/SPIDER_modelo_especificacaotecnologia.doc" >• Especifica&ccedil;&atilde;o T&eacute;cnica da Linguagem de Modelagem</a>
+	  - Template usado pelo grupo como instrumento para descrever tecnicamente as defini&ccedil;&otilde;es dos elementos que comp&otilde;em a linguagem de modelagem do	 	processo de software.
+	  <br /><br /><br />
+
+    <a download id="content_title2" href="templates/SPIDER_modelo_caracteristicasempresa-projeto.doc" >• Formul&aacute;rio de Caracter&iacute;sticas da Empresa</a>
+	  - Template usado pelo grupo para coletar dados da empresas.
+	  <br />
+	  <br /><br />
+
+    <a download id="content_title2" href="templates/SPIDER_modelo_listafrequencia.doc" >• Lista de Frequ&ecirc;ncia</a>
+	  - Template usado pelo grupo para registrar a frequ&ecirc;ncia dos membros do SPIDER.
+	  <br />
+	  <br /><br />
+
+    <a download id="content_title2" href="templates/SPIDER_modelo_listafrequenciaconsultoria.doc" >• Lista de Frequ&ecirc;ncia para Consultoria</a>
+	  - Template usado pelo grupo para registrar a frequ&ecirc;ncia das pessoas presentes nos eventos.
+	  <br />
+	  <br /><br />
+
+	  <a download id="content_title2" href="templates/SPIDER_modelo_manualimplementacaoferramenta.doc" >• Manual de Implementa&ccedil;&atilde;o de Processos</a>
+	  - Template usado pelo grupo como instrumento para descrever todas as premissas definidas para uso das ferramentas de apoio na implementa&ccedil;&atilde;o dos 		Processos do MPS.BR.
+	  <br /><br /><br />
+
+	  <a download id="content_title2" href="templates/SPIDER_modelo_manualimplementacaointegrada.doc" >• Manual de Implementa&ccedil;&atilde;o Integrada de Processos</a>
+	  - Template usado pelo grupo como instrumento para descrever todas as premissas definidas para uso das ferramentas de apoio na implementa&ccedil;&atilde;o 			integrada dos Processos do MPS.BR.
+	  <br /><br /><br />
+
+    <a download id="content_title2" href="templates/SPIDER_modelo_manualusuario.doc" >• Manual do Usu&aacute;rio</a>
+	  - Template usado pelo grupo como instrumento para descrever todas as premissas definidas para uso do sistema/ferramenta desenvolvido(a).
+	  <br /><br /><br />
+
+    <a download id="content_title2" href="templates/SPIDER_modelo_dependenciaresultados.doc" >• Mapeamento dos Resultados Esperados</a>
+	  - Template usado pelo grupo como instrumento para mapear os Resultados Esperados dos Processos constantes no modelo MPS.BR.
+	  <br /><br /><br />
+
+    <a download id="content_title2" href="templates/SPIDER_modelo_planilhadiagnostico.xls" >• Planilha de Diagn&oacute;sticos</a>
+	  - Template usado pelas empresas para catalogar os resultados esperados com a implanta&ccedil;&atilde;o da Ger&ecirc;ncia de Projetos.<br />
+	  <br /><br />
+
+    <a download id="content_title2" href="templates/SPIDER_modelo_questionarioavaliacao.doc" >• Question&aacute;rio de Avalia&ccedil;&atilde;o</a>
+	  - Template usado pelo grupo para avaliar os processos das empresas.<br />
+	  <br /><br />
+
+    <a download id="content_title2" href="templates/SPIDER_modelo_relatorioatividades.doc" >• Relat&oacute;rio de Atividades de Pesquisa</a>
+	  - Template usado pelo grupo para o registro das atividades de pesquisa realizadas, bem como os resultados obtidos e pr&oacute;ximas a&ccedil;&otilde;es.
+	  <br /><br /><br />
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
