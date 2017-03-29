d7c902c4a6ec5fa5b2755e22151a4a48844050ed
lubien <lubien1996@gmail.com>
2017-03-28 22:39:54 -0300

    Add subprojects page

---
+++ pages/subprojects.vue
@@ -0,0 +1,198 @@
+<template>
+  <div>
+    <script type="text/javascript">
+    // eslint-disable-next-line
+    function setTable (what){
+      if (document.getElementById(what).style.display === 'none') {
+        document.getElementById(what).style.display = 'block'
+      } else if (document.getElementById(what).style.display === 'block') {
+        document.getElementById(what).style.display = 'none'
+      }
+    }
+    </script>
+
+    <a id="content_title2" href="#" onclick="setTable('andamento');return false">
+      <div id="content_title"> Em Andamento+</div><hr /><br />
+    </a>
+
+    <table id="andamento" style="display:none;">
+
+      <tr><td>
+          <div id="content_title2">
+            <a id="content_title2">•	Educação em Engenharia de Software com foco em MPS.</a>
+          </div>
+          <b>Responsáveis:</b> Carlos Portela.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">•	Educação em Controle Estatístico de Processo em MPS.</a>
+          </div>
+          <b>Responsáveis:</b> Júlio Furtado.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">•	Aquisição com Métodos Ágeis.</a>
+          </div>
+          <b>Responsáveis:</b>Melquizedequi Santos.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">•	Ensino de Fábricas de Software.</a>
+          </div>
+          <b>Responsáveis:</b> Marcela de Souza.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">•	Desenvolvimento de Requisitos com Métodos Ágeis.</a>
+          </div>
+          <b>Responsáveis:</b> Kleoson Bruno.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">•	Catálogo de Abordagens para Controle Estatístico de Processo em MPS.</a>
+          </div>
+          <b>Responsáveis:</b> Aluizio Ramos.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">•	Gerência de Portfólio de Projetos com Métodos Ágeis.</a>
+          </div>
+          <b>Responsáveis:</b> Lilian Ferreira.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">•	Apoio à Gestão de Decisão do MPS.</a>
+          </div>
+          <b>Responsáveis:</b> Luiz Danin.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">•	Catálogo de Abordagens de Aquisição com Métodos Ágeis.</a>
+          </div>
+          <b>Responsáveis:</b> Elisiane Soares.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">•	Revisão Sistemática de Controle Estatístico de Processo em MPS.</a>
+          </div>
+          <b>Responsáveis:</b> Arthur Moraes e Géssica Pinheiro.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">•	Spider-DAR: Ferramenta de Apoio à Gestão de Decisão.</a>
+          </div>
+          <b>Responsáveis:</b> Bleno Vale.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">•	Revisão Sistemática de Ativos de Medição.</a>
+          </div>
+          <b>Responsáveis:</b> Rubens Rocha.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">•	Revisão Sistemática de Desenvolvimento de Requisitos com Métodos Ágeis.</a>
+          </div>
+          <b>Responsáveis:</b> Adilson Sousa e Victor Rodrigues.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">•	Ferramentas de Apoio ao Controle Estatístico de Processo em MPS.</a>
+          </div>
+          <b>Responsáveis:</b> Diego Abreu.<br /><br/>
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('concluido');return false">
+      <div id="content_title"> Concluídos+</div><hr /><br />
+    </a>
+
+    <table id="concluido" style="display:none;">
+
+      <tr><td>
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado aos Processos de Definição, Avaliação e Melhoria de Processo.</a>
+          </div>
+          <b>Responsáveis:</b> Leonardo e Fabrício Araújo.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Implementação do Processo de Gerência de Recursos Humanos do MPS.BR.</a>
+          </div>
+          <b>Responsáveis:</b> Wallace Lira, Laís, Paulo Malcher e Ehilton.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Implementação do Processo  de Gerência de Portfólio do MPS.BR.</a>
+          </div>
+          <b>Responsáveis:</b>Maurício Ronny, Ariane e Rousevelt.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Implementação do Processo  de Gerência de Reutilização do MPS.BR.</a>
+          </div>
+          <b>Responsáveis:</b> Fabrício Alho e Kleverton.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Implementação do Processo  de Avaliação do MPS.BR.</a>
+          </div>
+          <b>Responsáveis:</b> Jñane e Pedro Aviz.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Execução de Processo de  Software.</a>
+          </div>
+          <b>Responsáveis:</b> Carlos Portela, Mariano, Helder e André.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado a Simulação de Processos de  Software.</a>
+          </div>
+          <b>Responsáveis:</b> Rafael Chaves.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Integração de Ferramentas  no Contexto de Melhoria de Processo.</a>
+          </div>
+          <b>Responsáveis:</b> Ewelton.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Manual de Implementação do Processo de Garantia  da Qualidade.</a>
+          </div>
+          <b>Responsáveis:</b> Rodrigo Barbário.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Manual de Implementação do Processo de Medição.</a>
+          </div>
+          <b>Responsáveis:</b> Simone Carneiro.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Manual de Implementação do Processo de Aquisição.</a>
+          </div>
+          <b>Responsáveis:</b> Rodrigo e Fernando.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Implementação do Processo  de Medição do MPS.BR.</a>
+          </div>
+          <b>Responsáveis:</b> Bernardo Estácio e Kaio.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Implementação do Processo  de Aquisição do MPS.BR.</a>
+          </div>
+          <b>Responsáveis:</b> Júlio.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Implementação do Processo  de Garantia da Qualidade do MPS.BR.</a>
+          </div>
+          <b>Responsáveis:</b> Marília.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Estimativa de Projetos.</a>
+          </div>
+          <b>Responsáveis:</b> Gleise, Sivaldinho e Kleverton.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Implementação do Processo  de Gerência de Projetos do MPS.BR</a>
+          </div>
+          <b>Responsáveis:</b> Miguel, Vitor, Mauricio, Wallace, Ewelton e  Pedro.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Implementação do Processo  de Gerência de Requisitos do MPS.BR.</a>
+          </div>
+          <b>Responsáveis:</b> Alexandre e Luciana.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Implementação do Processo  de Desenvolvimento de Requisitos do MPS.BR.</a>
+          </div>
+          <b>Responsáveis:</b> Ewelton.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Implementação do Processo  de Gerência de Configuração do MPS.BR.</a>
+          </div>
+          <b>Responsáveis:</b> Ewelton e Maurício.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Análise Integrada dos Resultados Esperados do  Nível G do MPS.BR.</a>
+          </div>
+          <b>Responsáveis:</b> Wallace e Paulo Rudolph.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Análise Integrada dos Resultados Esperados do  Nível F do MPS.BR.</a>
+          </div>
+          <b>Responsáveis:</b> Alberto e Heresson.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Adaptação do Processo de Gerência de Projetos do  CTIC, usando Metodologias Ágeis.</a>
+          </div>
+          <b>Responsáveis:</b> Carlos Portela.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Avaliação de Produtos de  Software Baseado no MEDE-PROS.</a>
+          </div>
+          <b>Responsáveis:</b> Gleyson Gama.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Modelagem de Processos de  Software.</a>
+          </div>
+          <b>Responsáveis:</b> Renan Sales.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Manutenção de Ativos do  MPS-BR.</a>
+          </div>
+          <b>Responsáveis:</b> Pedro.<br /><br />
+          <div id="content_title2">
+            <a id="content_title2">• Apoio Automatizado à Geração de Checklists para  Avaliação Objetiva.</a>
+          </div>
+          <b>Responsáveis:</b> Renan Sales.<br /><br />
+        </td></tr>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
