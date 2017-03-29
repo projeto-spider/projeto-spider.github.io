a894d086fca37a3d662d00280f307fb5cef44284
lubien <lubien1996@gmail.com>
2017-03-28 22:37:34 -0300

    Add reasearch page

---
+++ pages/research.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div id="content_title2">• Qualidade de Software</div><br /><br />
+    <div id="content_title2">• Tecnologia de Processos de Software</div><br /><br />
+    <div id="content_title2">• Gerência de Projetos de Software</div><br /><br />
+    <div id="content_title2">• Sistemas de Informação</div><br /><br />
+    <div id="content_title2">• Revisão Sistemática</div><br /><br/>
+    <div id="content_title2">• Educação em Engenharia de Software</div><br />
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
