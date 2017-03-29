2016d37018873645317834edd73a6bc0ecbe825e
lubien <lubien1996@gmail.com>
2017-03-28 23:40:05 -0300

    Add sitemap page

---
+++ pages/sitemap.vue
@@ -0,0 +1,167 @@
+<template>
+  <div>
+    <div id="map_root">
+		  <img id="map_left_top" src="~assets/images/site_map/root/left_top.png" />
+		  <img id="map_right_top" src="~assets/images/site_map/root/right_top.png" />
+		  <img id="map_left_bottom" src="~assets/images/site_map/root/left_bottom.png" />
+		  <img id="map_right_bottom" src="~assets/images/site_map/root/right_bottom.png" />
+		  <div id="map_text">
+			  <a id="map_root_link" href="index.php?id=inicio">
+				  In&iacute;cio
+			  </a>
+		  </div>
+	  </div>
+	  <div id="map_firt_level">
+		  <div id="map_line_a">
+		  </div>
+		  <img id="map_left_top" src="~assets/images/site_map/first_level/left_top.png" />
+		  <img id="map_right_top" src="~assets/images/site_map/first_level/right_top.png" />
+		  <img id="map_left_bottom" src="~assets/images/site_map/first_level/left_bottom.png" />
+		  <img id="map_right_bottom" src="~assets/images/site_map/first_level/right_bottom.png" />
+		  <div id="map_text">
+			  <a id="map_first_level_link" href="index.php?id=sobre">
+				  Sobre
+			  </a>
+		  </div>
+	  </div>
+	  <div id="map_firt_level">
+		  <div id="map_line_a">
+		  </div>
+		  <img id="map_left_top" src="~assets/images/site_map/first_level/left_top.png" />
+		  <img id="map_right_top" src="~assets/images/site_map/first_level/right_top.png" />
+		  <img id="map_left_bottom" src="~assets/images/site_map/first_level/left_bottom.png" />
+		  <img id="map_right_bottom" src="~assets/images/site_map/first_level/right_bottom.png" />
+		  <div id="map_text">
+			  <a id="map_first_level_link" href="index.php?id=equipe">
+				  Equipe
+			  </a>
+		  </div>
+	  </div>
+	  <div id="map_firt_level">
+		  <div id="map_line_a">
+		  </div>
+		  <img id="map_left_top" src="~assets/images/site_map/first_level/left_top.png" />
+		  <img id="map_right_top" src="~assets/images/site_map/first_level/right_top.png" />
+		  <img id="map_left_bottom" src="~assets/images/site_map/first_level/left_bottom.png" />
+		  <img id="map_right_bottom" src="~assets/images/site_map/first_level/right_bottom.png" />
+		  <div id="map_text">
+			  <a id="map_first_level_link" href="index.php?id=pesquisa">
+				  Linhas de Pesquisa
+			  </a>
+		  </div>
+	  </div>
+	  <div id="map_firt_level">
+		  <div id="map_line_a">
+		  </div>
+		  <img id="map_left_top" src="~assets/images/site_map/first_level/left_top.png" />
+		  <img id="map_right_top" src="~assets/images/site_map/first_level/right_top.png" />
+		  <img id="map_left_bottom" src="~assets/images/site_map/first_level/left_bottom.png" />
+		  <img id="map_right_bottom" src="~assets/images/site_map/first_level/right_bottom.png" />
+		  <div id="map_text">
+			  <a id="map_first_level_link" href="index.php?id=subprojetos">
+				  Subprojetos
+			  </a>
+		  </div>
+	  </div>
+
+	  <div id="map_firt_level">
+		  <div id="map_line_a">
+		  </div>
+		  <img id="map_left_top" src="~assets/images/site_map/first_level/left_top.png" />
+		  <img id="map_right_top" src="~assets/images/site_map/first_level/right_top.png" />
+		  <img id="map_left_bottom" src="~assets/images/site_map/first_level/left_bottom.png" />
+		  <img id="map_right_bottom" src="~assets/images/site_map/first_level/right_bottom.png" />
+		  <div id="map_text">
+			  <a id="map_first_level_link" href="index.php?id=resultados">
+				  Resultados
+			  </a>
+		  </div>
+	  </div>
+
+	  <div id="map_firt_level">
+		  <div id="map_line_a">
+		  </div>
+		  <img id="map_left_top" src="~assets/images/site_map/first_level/left_top.png" />
+		  <img id="map_right_top" src="~assets/images/site_map/first_level/right_top.png" />
+		  <img id="map_left_bottom" src="~assets/images/site_map/first_level/left_bottom.png" />
+		  <img id="map_right_bottom" src="~assets/images/site_map/first_level/right_bottom.png" />
+		  <div id="map_text">
+			  <a id="map_first_level_link" href="index.php?id=parcerias">
+				  Parcerias
+			  </a>
+		  </div>
+	  </div>
+	  <div id="map_firt_level">
+		  <div id="map_line_a">
+		  </div>
+		  <img id="map_left_top" src="~assets/images/site_map/first_level/left_top.png" />
+		  <img id="map_right_top" src="~assets/images/site_map/first_level/right_top.png" />
+		  <img id="map_left_bottom" src="~assets/images/site_map/first_level/left_bottom.png" />
+		  <img id="map_right_bottom" src="~assets/images/site_map/first_level/right_bottom.png" />
+		  <div id="map_text">
+			  <a id="map_first_level_link" href="index.php?id=noticias">
+				  Not&iacute;cias
+			  </a>
+		  </div>
+	  </div>
+	  <div id="map_firt_level">
+		  <div id="map_line_a">
+		  </div>
+		  <img id="map_left_top" src="~assets/images/site_map/first_level/left_top.png" />
+		  <img id="map_right_top" src="~assets/images/site_map/first_level/right_top.png" />
+		  <img id="map_left_bottom" src="~assets/images/site_map/first_level/left_bottom.png" />
+		  <img id="map_right_bottom" src="~assets/images/site_map/first_level/right_bottom.png" />
+		  <div id="map_text">
+			  <a id="map_first_level_link" href="index.php?id=templates">
+			    Templates SPIDER
+			  </a>
+		  </div>
+	  </div>
+	  <div id="map_firt_level">
+		  <div id="map_line_a">
+		  </div>
+		  <img id="map_left_top" src="~assets/images/site_map/first_level/left_top.png" />
+		  <img id="map_right_top" src="~assets/images/site_map/first_level/right_top.png" />
+		  <img id="map_left_bottom" src="~assets/images/site_map/first_level/left_bottom.png" />
+		  <img id="map_right_bottom" src="~assets/images/site_map/first_level/right_bottom.png" />
+		  <div id="map_text">
+			  <a id="map_first_level_link" href="index.php?id=publicacoes">
+				  Publica&ccedil;&otilde;es
+			  </a>
+		  </div>
+	  </div>
+	  <div id="map_firt_level">
+		  <div id="map_line_a">
+		  </div>
+		  <img id="map_left_top" src="~assets/images/site_map/first_level/left_top.png" />
+		  <img id="map_right_top" src="~assets/images/site_map/first_level/right_top.png" />
+		  <img id="map_left_bottom" src="~assets/images/site_map/first_level/left_bottom.png" />
+		  <img id="map_right_bottom" src="~assets/images/site_map/first_level/right_bottom.png" />
+		  <div id="map_text">
+			  <a id="map_first_level_link" href="index.php?id=bookmarks">
+				  BookMarks
+			  </a>
+		  </div>
+	  </div>
+	  <!--<div id="map_second_level">
+		 <div id="map_line_b">
+		 </div>
+		 <img id="map_left_top" src="~assets/images/site_map/second_level/left_top.png" />
+		 <img id="map_right_top" src="~assets/images/site_map/second_level/right_top.png" />
+		 <img id="map_left_bottom" src="~assets/images/site_map/second_level/left_bottom.png" />
+		 <img id="map_right_bottom" src="~assets/images/site_map/second_level/right_bottom.png" />
+	   </div>
+	   <div id="map_third_level">
+		 <div id="map_line_a">
+		 </div>
+		 <img id="map_left_top" src="~assets/images/site_map/third_level/left_top.png" />
+		 <img id="map_right_top" src="~assets/images/site_map/third_level/right_top.png" />
+		 <img id="map_left_bottom" src="~assets/images/site_map/third_level/left_bottom.png" />
+		 <img id="map_right_bottom" src="~assets/images/site_map/third_level/right_bottom.png" />
+	   </div>-->
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
