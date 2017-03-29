9250286f473f4d2b2a6ba4c8c5b93c3435bf1389
lubien <lubien1996@gmail.com>
2017-03-28 22:47:28 -0300

    Add partnerships page

---
+++ pages/partnerships.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+    <script type="text/javascript">
+    // eslint-disable-next-line
+    function setTable (what){
+      if (document.getElementById(what).style.display === 'none') {
+        document.getElementById(what).style.display = 'block'
+      } else if (document.getElementById(what).style.display === 'block') {
+        document.getElementById(what).style.display = 'none'
+      }
+    }
+    </script>
+
+    <a id="content_title2" href="#" onclick="setTable('gpesq');return false">
+      <div id="content_title"> Grupos de Pesquisa+</div><hr /><br />
+    </a>
+
+    <table id="gpesq" style="display:none;">
+      <tr><td>
+          <a target="_blanck" href="http://www.cin.ufpe.br/~imppros">
+            <img border="0px" src="~assets/images/logos/imppros.png">
+          </a><br /><b>Grupo de Processos de Desenvolvimento de Software - UFPE</b>
+          <br /><br /><br />
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('inst');return false">
+      <div id="content_title"> Instituições de Ensino+</div><hr /><br />
+    </a>
+
+    <table id="inst" style="display:none;">
+      <tr><td>
+          <a target="_blanck" href="http://www.cin.ufpe.br">
+            <img border="0px" src="~assets/images/logos/cin-ufpe.png">
+          </a><br /><b>Centro de Informática da UFPE.</b>
+          <br /><br /><br />
+
+          <a target="_blanck" href="http://www.ufrpe.br">
+            <img border="0px" src="~assets/images/logos/ufrpe.png">
+          </a><br /><b>UFRPE - Universidade Federal Rural de Pernambuco.</b>
+          <br /><br /><br />
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('fomep');return false">
+      <div id="content_title"> Fomentos de Pesquisa+</div><hr /><br />
+    </a>
+
+    <table id="fomep" style="display:none;">
+      <tr><td>
+          <a target="_blanck" href="http://www.cnpq.br">
+            <img border="0px" src="~assets/images/logos/cnpq.png">
+          </a><br /><b>CNPq - Conselho Nacional de Desenvolvimento Científico e Tecnológico</b>
+          <br /><br /><br />
+
+          <a target="_blanck" href="http://www.capes.gov.br">
+            <img border="0px" src="~assets/images/logos/capes.png">
+          </a><br /><b>CAPES - Coordenação de Aperfeiçoamento de Pessoal de Nível Superior</b>
+          <br /><br /><br />
+
+          <a target="_blanck" href="http://www.pibic.ufpa.br">
+            <img border="0px" src="~assets/images/logos/pibic-ufpa.png">
+          </a><br /><b>PIBIC/UFPA - Programa Integrado de Bolsas de Iniciação Científica.</b>
+          <br /><br /><br />
+        </td></tr>
+    </table>
+
+    <a id="content_title2" href="#" onclick="setTable('empre');return false">
+      <div id="content_title"> Empresas Parceiras+</div><hr /><br />
+    </a>
+
+    <table id="empre" style="display:none;">
+      <tr><td>
+          <a target="_blanck" href="http://www.swquality.com.br">
+            <img border="0px" src="~assets/images/logos/swquality.png">
+          </a><br /><b>Consultoria e Capacitação em Melhoria do Processo Organizacional</b>
+          <br /><br /><br />
+
+          <a target="_blanck" href="http://www.proqualiti.org.br">
+            <img border="0px" src="~assets/images/logos/proqualiti.png">
+          </a><br /><b>Núcleo de Estudos em Engenharia e Qualidade de Software</b>
+          <br /><br /><br />
+
+          <a target="_blanck" href="http://www.fabsoft.cesupa.br/site/index.php">
+            <img border="0px" src="~assets/images/logos/fabSoft.png">
+          </a><br /><b>FabSoft - CESUPA</b>
+          <br /><br /><br />
+
+          <a target="_blanck" href="http://www.prontodigital.com.br/">
+            <img border="0px" src="~assets/images/logos/pronto-digital.png">
+          </a><br /><b>Pronto Digital</b>
+          <br /><br /><br />
+
+          <a target="_blanck" href="http://www.ifpa.edu.br/">
+            <img border="0px" src="~assets/images/logos/IFPA.png">
+          </a><br /><b>IFPA - Instituto Federal do Pará</b>
+          <br /><br/><br/>
+
+          <a target="_blanck" href="http://www.jambu.com.br">
+            <img border="0px" src="~assets/images/logos/jambu.png">
+          </a><br /><b>Jambu Tecnologia.</b>
+          <br /><br /><br />
+
+          <a target="_blanck" href="http://www.golsoftware.com.br">
+            <img border="0px" src="~assets/images/logos/gol.png">
+          </a><br /><b>Gol Software.</b>
+          <br /><br /><br />
+
+          <a target="_blanck" href="http://www.vibedesenv.com">
+            <img border="0px" src="~assets/images/logos/vibe.png">
+          </a><br /><b>Vibe Desenvolvimento.</b>
+          <br /><br /><br />
+
+          <a target="_blanck" href="http://www.emprel.gov.br/">
+            <img border="0px" src="~assets/images/logos/emprel.png">
+          </a><br /><b>EMPREL - Empresa Municipal de Informática.</b>
+          <br /><br /><br />
+
+          <a target="_blanck" href="http://www.tre-ma.jus.br/">
+            <img border="0px" src="~assets/images/logos/tre-ma.png">
+          </a><br /><b>TRE/MA - Tribunal Regional Eleitoral do Maranhão.</b>
+          <br /><br/><br/>
+
+          <a target="_blanck" href="http://www.fucapi.br">
+            <img border="0px" src="~assets/images/logos/fucapi.png">
+          </a><br /><b>FUCAPI - Fundação Centro de Análise, Pesquisa e Inovação Tecnológica.</b>
+          <br /><br /><br />
+
+          <a target="_blanck" href="http://www.cpc.pa.gov.br/">
+            <img border="0px" src="~assets/images/logos/renato-chaves.png">
+          </a><br /><b>CPC - Centro de Perícias Científicas Renato Chaves.</b>
+          <br /><br /><br />
+
+          <a target="_blanck" href="http://www.softcomtecnologia.com.br">
+            <img border="0px" src="~assets/images/logos/softcom.png">
+          </a><br /><b>Softcom Tecnologia.</b>
+          <br /><br /><br />
+
+          <a target="_blanck" href="http://www.egen.com.br/site/">
+            <img border="0px" src="~assets/images/logos/e-gen.png">
+          </a><br /><b>Grupo E-Gen.</b>
+          <br /><br /><br />
+        </td></tr>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
