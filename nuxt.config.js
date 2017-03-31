65feddc2438c440b7afaf0c03ba693a41fb3a6d8
lubien <lubien1996@gmail.com>
2017-03-30 21:32:52 -0300

    Update head tag data

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,14 +3,14 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'starter',
+    title: 'Projeto SPIDER',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Software Process Improvement: Development and Research' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
   /*
